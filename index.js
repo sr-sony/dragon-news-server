741f92143bcf52d0737c6ffe5b2df3e013d2930d
sr-sony <sr.sony.05@gmail.com>
2022-10-20 19:50:16 +0600

    added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News API running');
@@ -14,6 +15,29 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req,res) => {
     res.send(categories);
+});
+
+app.get('/category/:id', (req,res) => {
+    const id = req.params.id;
+    if(id === '08'){
+        res.send(news);
+    }
+    else{
+        const category_news = news.filter(n=> n.category_id === id);
+    res.send(category_news);
+    }
+    
+})
+
+app.get('/news', (req,res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req,res)=> {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+    console.log(req.params)
 })
 
 app.listen(port, () => {
